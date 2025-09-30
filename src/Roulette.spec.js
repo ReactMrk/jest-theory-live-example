654dc78371f6ca0290cfdef707191a28200cbfea
SCP13 <sergi.campos2@sky.uk>
2025-09-30 08:43:55 +0100

    SCC | add comments for solution approach

--- src/Roulette.spec.js
+++ src/Roulette.spec.js
@@ -8,24 +8,24 @@ const renderComponent = () => render(<Roulette/>);
 describe('Roulette', () => {
     it("renders a roll button", () => {
         renderComponent();
-        expect(screen.getByRole('button', { name: 'Roll the roulette' })).toBeVisible()
+        expect(screen.getByRole('button', {name: 'Roll the roulette'})).toBeVisible()
     });
 
     it.skip("renders a restart button", () => {
-
+        // show other ways to test like data test id or get by text
     });
 
     describe.skip("roll the roulette", () => {
         it("given a high roll shows win a message", () => {
-
+            // here mock the utils file that generates the random number
         });
 
         it("given a low roll shows win a message", () => {
-
+            // here mock the utils file that generates the random number
         });
     });
 
     it.skip("restart clears the result", () => {
-
+    // negative testing (show that the element is not present)
     });
 })
